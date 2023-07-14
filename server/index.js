a0136c3845d4a2dce219d00b3d6dc631fe3575fd
Abhyuday-Dev <abhyuday7176@hotmail.com>
2023-07-14 22:09:54 +0530

    changes in index.js

--- server/index.js
+++ server/index.js
@@ -4,15 +4,17 @@ require("./MongoDB/connect");
 const bookRoute=require("./routes/booksRoutes");
 const cors=require("cors");
 
+
+const PORT=process.env.PORT ||8080;
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1",bookRoute);
 
-// Wait for MongoDB connection before starting the server
+
 mongoose.connection.once("open", () => {
-  app.listen(8080, () => {
-    console.log("Server started on port 8080");
+  app.listen(PORT, () => {
+    console.log(`Server Started on port ${PORT}`);
   });
 });
