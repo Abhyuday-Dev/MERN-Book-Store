82b90cdea56b776b0783bff1582e9e9f9c682dca
Abhyuday-Dev <abhyuday7176@hotmail.com>
2023-07-14 23:02:07 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -1,17 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
 require("./MongoDB/connect");
-const bookRoute=require("./routes/booksRoutes");
-const cors=require("cors");
+const bookRoute = require("./routes/booksRoutes");
+const cors = require("cors");
 
-
-const PORT=process.env.PORT ||8080;
+const PORT = process.env.PORT || 8080;
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/v1",bookRoute);
-
+app.use("/api/v1", bookRoute);
 
 mongoose.connection.once("open", () => {
   app.listen(PORT, () => {
